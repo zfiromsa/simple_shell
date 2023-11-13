58488a0f9575f143b1bc84fdabf3c31f2f975ade
zfiromsa <mdzm841@gmail.com>
2023-11-14 00:27:21 +0300

    go

--- main.h
+++ main.h
@@ -18,7 +18,7 @@ struct str_list *next;
 
 _str_list **lists_cmd(char *cmd, _str_list **cmds);
 void free_list(_str_list **head);
-void exec_list(_str_list *head);
+void exec_list(_str_list *head, char **envp);
 _str_list *add_nodeint_end(_str_list **head, char *cmd);
 
 #endif
