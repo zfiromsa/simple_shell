404b5e96de8df064c13a3a4832f20441dd3259bb
zfiromsa <mdzm841@gmail.com>
2023-11-14 09:30:00 +0300

    go

--- list_cmd.c
+++ list_cmd.c
@@ -14,23 +14,28 @@ char **Func_list_cmds(char *cmds, char **list_cmds)
         return (NULL);
     }
     token = strtok(cmds, " ");
+    printf("test 1:\t %s\n", token);
     while (token != NULL)
     {
         if (token[0] == '#')
         {
+ printf("test 2:\t %s\n", token);
             break;
         }
         if (strcmp(token, "exit") || strcmp(token, "EXIT"))
         {
+ printf("test 3:\t %s\n", token);
             return (0);
         }
         list_cmds[i] = strdup(token);
+ printf("test %d:\t %s\n", i, list_cmds[i]);
         if (list_cmds == NULL)
         {
             perror("Error func");
             break;
         }
         token = strtok(NULL, " ");
+ printf("token test %d:\t %s\n", i, token);
         i++;
     }
     free(cmds);
