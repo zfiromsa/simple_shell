c589d8e466c453422e157c2f9e7f7f4715317696
zfiromsa <mdzm841@gmail.com>
2023-11-14 11:09:01 +0300

    go

--- _free.c
+++ _free.c
@@ -2,13 +2,13 @@
 
 void _free(char **list_cmds)
 {
-    char **list;
+    char *list;
     int i;
 
     i = 0;
     while (list_cmds[i] != NULL)
     {
-        list = list_cmds[i];
+        list = list_cmds;
         free(list_cmds[i]);
         i++;
     }
