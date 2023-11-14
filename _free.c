4b9690da60539fdc04090228d1d0193ac8e20973
zfiromsa <mdzm841@gmail.com>
2023-11-14 11:07:28 +0300

    go

---
+++ _free.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void _free(char **list_cmds)
+{
+    char **list;
+    int i;
+
+    i = 0;
+    while (list_cmds[i] != NULL)
+    {
+        list = list_cmds[i];
+        free(list_cmds[i]);
+        i++;
+    }
+    free(list_cmds);
+}
+
