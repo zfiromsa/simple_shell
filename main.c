e9b708ce9491e0281a3eb302822d454449e3e6d3
zfiromsa <mdzm841@gmail.com>
2023-11-15 00:56:04 +0300

    go

--- main.c
+++ main.c
@@ -25,7 +25,6 @@ int main(int argc, char **argv, char **env)
 		list_cmds = Func_list_cmds(cmds, list_cmds);
 		if (list_cmds == NULL)
 		{
-			perror("main Error ");
 			break;
 		}
 		_status = execute(list_cmds, env);
