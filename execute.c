9508d6e95296f7417937fb2be6116d6444e63566
zfiromsa <mdzm841@gmail.com>
2023-11-15 23:03:50 +0300

    go

--- execute.c
+++ execute.c
@@ -20,9 +20,6 @@ int execute(char **list_cmds, char **env)
 	}
 	else if (pid == 0)
 	{
-		close(STDIN_FILENO);
-		close(STDOUT_FILENO);
-		close(STDERR_FILENO);
 		if (execve(list_cmds[0], list_cmds, env) == -1)
 		{
 			perror("Error exc");
