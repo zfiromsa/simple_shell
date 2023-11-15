2e4b2584141433774f8d7ab100e4b1d6080f7d88
zfiromsa <mdzm841@gmail.com>
2023-11-15 17:14:36 +0300

    go

--- list_cmd.c
+++ list_cmd.c
@@ -25,7 +25,7 @@ char **Func_list_cmds(char *cmds, char **list_cmds)
 		{
 			break;
 		}
-		list_cmds[i] = strdrup(token);
+		list_cmds[i] = strdup(token);
 		i++;
 		list_cmds = realloc(list_cmds, ((i + 1) * sizeof(char *)));
 		if (list_cmds == NULL)
